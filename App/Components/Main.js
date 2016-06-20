8460807765090455ce72597ef75b473ed68b1357
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-20 10:52:35 +0100

    YL - add text input and search button in Main page

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -1,20 +1,52 @@
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     StyleSheet,
     Text,
     View,
+    TextInput,
+    TouchableHighlight,
+    ActivityIndicatorIOS,
 } from 'react-native';
 
 class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            isLoading: false,
+            error: false
+        }
+    }
+
     render() {
         return (
             <View style={styles.mainContainer}>
-                <Text>
-                    test router
-                </Text>
+                <Text style={styles.title}>Search for a Github User</Text>
+
+                <TextInput
+                    style={styles.searchInput}
+                    value={this.state.username}
+                    onChange={this.handleChange.bind(this)}/>
+
+                <TouchableHighlight style={styles.button} onPress={this.handleSubmit.bind(this)} underlayColor="white">
+                    <Text style={styles.searchText}> SEARCH </Text>
+                </TouchableHighlight>
             </View>
         );
+    }
+
+    handleChange(event) {
+        this.setState({
+            username: event.nativeEvent.text,
+        });
+    }
+
+    handleSubmit() {
+        this.setState({
+            isLoading: true,
+        });
+
+        console.log("submit");
     }
 }
 
@@ -34,6 +66,32 @@ var styles = StyleSheet.create({
         textAlign: 'center',
         color: '#ffffff',
     },
+
+    searchInput: {
+        height: 45,
+        width: 300,
+        borderColor: '#ffffff',
+        borderRadius: 5,
+        borderWidth: 1,
+        alignSelf: 'center',
+    },
+
+    button: {
+        height: 45,
+        width: 300,
+        marginTop: 10,
+        backgroundColor: '#ffffff',
+        borderColor: '#ffffff',
+        borderRadius: 5,
+        borderWidth: 1,
+        alignSelf: 'center',
+        justifyContent: 'center',
+    },
+
+    searchText: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+    }
 
 });
 
