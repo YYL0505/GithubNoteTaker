9cd9718d7e7fa088eb9fdc0e7cf372408ab94984
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-21 09:38:24 +0100

    YL - add api to get notes and save notes.

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -13,7 +13,26 @@ var api = {
         var url = 'https://api.github.com/users/' + username;
 
         return fetch(url).then((response) => response.json());
-    }
+    },
+
+    getNotes(username) {
+        username = username.toLowerCase().trim();
+
+        var url = 'https://githubnotetaker-5a776.firebaseio.com/' + username + '.json';
+
+        return fetch(url).then((response) => response.json());
+    },
+
+    addNote(username, note) {
+        username = username.toLowerCase().trim();
+
+        var url = 'https://githubnotetaker-5a776.firebaseio.com/' + username + '.json';
+
+        return fetch(url, {
+            method: 'post',
+            body: JSON.stringify(note)
+        }).then((response) => response.json());
+    },
 };
 
 module.exports = api;
