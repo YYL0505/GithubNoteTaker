81afd785e881f8774cf7db3c0a79753809e81f2e
Yingli Yuan <ylyuan@thoughtworks.com>
2016-07-07 09:47:37 +0100

    YL - abstract AddNote from Note.

--- App/Components/Note.js
+++ App/Components/Note.js
@@ -3,15 +3,13 @@ import {
     StyleSheet,
     Text,
     View,
-    TextInput,
-    TouchableHighlight,
     ListView,
     Platform
 } from 'react-native';
 import {connect} from 'react-redux';
 import Spinner from 'react-native-loading-spinner-overlay';
 
-import api from '../Utils/api';
+import AddNote from '../containers/AddNote';
 import Badge from './Badge';
 import Separator from './Helpers/Separator';
 
@@ -21,8 +19,7 @@ class Note extends Component {
     }
 
     render() {
-        const {state, dispatch} = this.props;
-        var stateNote = state.default.note;
+        const stateNote = this.props.state.default.note;
         return(
             <View style={styles.container}>
                 <ListView
@@ -31,7 +28,7 @@ class Note extends Component {
                     enableEmptySections = {true}
                     renderHeader={this.renderHeader.bind(this)}/>
 
-                {this.footer(stateNote, dispatch)}
+                <AddNote userInfo={this.props.userInfo} />
 
                 <View style={styles.spinnerContainer}>
                     <Spinner visible={stateNote.isLoading}/>
@@ -56,57 +53,6 @@ class Note extends Component {
             <Badge userInfo={this.props.userInfo}/>
         );
     }
-
-    footer(stateNote, dispatch) {
-        return (
-            <View style={styles.footContainer}>
-                <TextInput
-                    style={styles.searchInput}
-                    value={stateNote.note}
-                    onChange={this.handleNoteChange.bind(this, dispatch)}
-                    placeHolder="New Note"/>
-
-                <TouchableHighlight
-                    style={styles.button}
-                    onPress={this.submitNote.bind(this, stateNote, dispatch)}
-                    underlayColor="#88D4F5">
-                    <Text style={styles.buttonText}> Submit </Text>
-                </TouchableHighlight>
-            </View>
-        );
-    }
-
-    handleNoteChange(dispatch, event) {
-        dispatch({
-            type: 'SET_NOTE',
-            note: event.nativeEvent.text,
-        });
-    }
-
-    submitNote(stateNote, dispatch) {
-        dispatch({
-            type: 'TOGGLE_LOADING',
-        });
-
-        api.addNote(this.props.userInfo.login, stateNote.note)
-            .then(() => {
-                api.getNotes(this.props.userInfo.login)
-                    .then((response) => {
-                        dispatch({
-                            type: 'FETCH_NOTES',
-                            notes: response,
-                        });
-                    })
-                    .catch((error) => {
-                        dispatch({
-                            type: 'TOGGLE_LOADING',
-                        });
-
-                        console.log('Request failed' + error);
-                    });
-            })
-    }
-
 }
 
 
@@ -125,35 +71,8 @@ var styles = StyleSheet.create({
         transform: [{'translate':[0,0,1]}],
     },
 
-    buttonText: {
-        fontSize: 18,
-        color: '#FFFFFF',
-    },
-
-    button: {
-        height: 60,
-        backgroundColor: '#48BBEC',
-        flex: 3,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-    searchInput: {
-        height: 60,
-        padding: 10,
-        fontSize: 18,
-        color: '#111111',
-        flex: 10,
-    },
-
     rowContainer: {
         padding: 10,
-    },
-
-    footContainer: {
-        backgroundColor: '#E3E3E3',
-        alignItems: 'center',
-        flexDirection: 'row',
     },
 });
 
---
+++ App/containers/AddNote.js
@@ -0,0 +1,114 @@
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableHighlight
+} from 'react-native';
+import {connect} from 'react-redux';
+
+import api from '../Utils/api';
+
+class AddNote extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {state, dispatch} = this.props;
+        var stateNote = state.default.note;
+        return (
+            <View style={styles.footContainer}>
+                <TextInput
+                    style={styles.searchInput}
+                    value={stateNote.note}
+                    onChange={this.handleNoteChange.bind(this, dispatch)}
+                    placeHolder="New Note"/>
+
+                <TouchableHighlight
+                    style={styles.button}
+                    onPress={this.submitNote.bind(this, stateNote, dispatch)}
+                    underlayColor="#88D4F5">
+                    <Text style={styles.buttonText}> Submit </Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+    
+    handleNoteChange(dispatch, event) {
+        dispatch({
+            type: 'SET_NOTE',
+            note: event.nativeEvent.text,
+        });
+    }
+
+    submitNote(stateNote, dispatch) {
+        dispatch({
+            type: 'TOGGLE_LOADING',
+        });
+
+        api.addNote(this.props.userInfo.login, stateNote.note)
+            .then(() => {
+                api.getNotes(this.props.userInfo.login)
+                    .then((response) => {
+                        dispatch({
+                            type: 'FETCH_NOTES',
+                            notes: response,
+                        });
+                    })
+                    .catch((error) => {
+                        dispatch({
+                            type: 'TOGGLE_LOADING',
+                        });
+
+                        console.log('Request failed' + error);
+                    });
+            })
+    }
+
+}
+
+
+AddNote.propTypes= {
+    userInfo: React.PropTypes.object.isRequired,
+};
+
+var styles = StyleSheet.create({
+    
+    buttonText: {
+        fontSize: 18,
+        color: '#FFFFFF',
+    },
+
+    button: {
+        height: 60,
+        backgroundColor: '#48BBEC',
+        flex: 3,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    searchInput: {
+        height: 60,
+        padding: 10,
+        fontSize: 18,
+        color: '#111111',
+        flex: 10,
+    },
+
+    footContainer: {
+        backgroundColor: '#E3E3E3',
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+});
+
+function selector(state) {
+    return {
+        state: state
+    }
+}
+export default connect(selector)(AddNote);
+
+
