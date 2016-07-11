e1568d0b0261226a8a511b311fca4b019fc3357b
Yingli Yuan <ylyuan@thoughtworks.com>
2016-07-11 12:07:14 +0100

    YL - add test for set note.

---
+++ tests/reducers/Note.spec.js
@@ -0,0 +1,31 @@
+import React from 'react-native';
+import {ListView} from 'react-native';
+import { assert } from 'chai';
+
+import note from '../../App/reducers/note'
+
+import { createStore } from 'redux';
+const store = createStore(note);
+
+describe('note reducer', () => {
+    it('should set note correctly and clear note when the note is empty', () => {
+      const noteText = 'this is my first note.';
+      var action = {
+        type: 'SET_NOTE',
+        note: noteText
+      };    
+      store.dispatch(action);
+      const actual = store.getState();
+      assert.propertyVal(actual, 'note', noteText);
+      
+      const emptyText = '';
+      action = {
+        type: 'SET_NOTE',
+        note: emptyText
+      };
+      store.dispatch(action);
+      const emptyActual = store.getState();
+      
+      assert.propertyVal(emptyActual, 'note', emptyText);
+    });
+});
